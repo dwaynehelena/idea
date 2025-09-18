28bb98cb2a975a71c955fe8540e7cbacb8b87a4b
Dwayne Helena <helenadw@cba.com.au>
2025-09-18 16:38:54 +1000

    Ignore build artifacts and outputs, remove large images from tracking

--- website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/helenadw/Documents/GitHub/idea/website/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/helenadw/Documents/GitHub/idea/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/helenadw/Documents/GitHub/idea/website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/helenadw/Documents/GitHub/idea/website/src/css/custom.css"),
-];
